c84fbd2883632b80e7e8cb27d7e9a0d537edeed1
Ryan <stander.ryan@gmail.com>
2022-05-07 14:44:05 +0200

    Pre-HDRP

--- Assets/Scripts/ModularMeshes/SimpleStock.cs
+++ Assets/Scripts/ModularMeshes/SimpleStock.cs
@@ -5,61 +5,54 @@ using UnityEngine;
 namespace Demo {
     public class SimpleStock : Shape {
         // grammar rule probabilities:
-        const float stockContinueChance = 0.5f;
+        private const float StockContinueChance = 0.5f;
 
         // shape parameters:
-        [SerializeField]
-        int Width;
-        [SerializeField]
-        int Depth;
+        [SerializeField] private int width;
+        [SerializeField] private int depth;
 
-        [SerializeField]
-        GameObject[] wallStyle;
-        [SerializeField]
-        GameObject[] roofStyle;
+        [SerializeField] private GameObject[] wallStyle;
+        [SerializeField] private GameObject[] roofStyle;
 
-        const float buildDelay = 0.1f;
+        private const float BuildDelay = 0.1f;
 		
-        public void Initialize(int Width, int Depth, GameObject[] wallStyle, GameObject[] roofStyle) {
-            this.Width=Width;
-            this.Depth=Depth;
+        public void Initialize(int width, int depth, GameObject[] wallStyle, GameObject[] roofStyle) {
+            this.width=width;
+            this.depth=depth;
             this.wallStyle=wallStyle;
             this.roofStyle=roofStyle;
         }
 
         protected override void Execute() {
             // Create four walls:
-            for (int i = 0; i<4; i++) {
-                Vector3 localPosition = new Vector3();
-                switch (i) {
-                    case 0:
-                        localPosition = new Vector3(-(Width-1)*0.5f, 0, 0); // left
-                        break;
-                    case 1:
-                        localPosition = new Vector3(0, 0, (Depth-1)*0.5f); // back
-                        break;
-                    case 2:
-                        localPosition = new Vector3((Width-1)*0.5f, 0, 0); // right
-                        break;
-                    case 3:
-                        localPosition = new Vector3(0, 0, -(Depth-1)*0.5f); // front
-                        break;
-                }
-                SimpleRow newRow = CreateSymbol<SimpleRow>("wall", localPosition, Quaternion.Euler(0, i*90, 0));
-                newRow.Initialize(i%2==1 ? Width : Depth,wallStyle);
+            for (var i = 0; i<4; i++) {
+                var localPosition = i switch
+                {
+                    0 => new Vector3(-(width - 1) * 0.5f, 0, 0) // left
+                    ,
+                    1 => new Vector3(0, 0, (depth - 1) * 0.5f) // back
+                    ,
+                    2 => new Vector3((width - 1) * 0.5f, 0, 0) // right
+                    ,
+                    3 => new Vector3(0, 0, -(depth - 1) * 0.5f) // front
+                    ,
+                    _ => new Vector3()
+                };
+                var newRow = CreateSymbol<SimpleRow>("wall", localPosition, Quaternion.Euler(0, i*90, 0));
+                newRow.Initialize(i%2==1 ? width : depth,wallStyle);
                 newRow.Generate();
             }		
 
             // Continue with a stock or with a roof (random choice):
-            float randomValue = RandomFloat();
-            if (randomValue < stockContinueChance) {
-                SimpleStock nextStock = CreateSymbol<SimpleStock>("stock", new Vector3(0, 1, 0));
-                nextStock.Initialize(Width, Depth, wallStyle, roofStyle);
-                nextStock.Generate(buildDelay);
+            var randomValue = RandomFloat();
+            if (randomValue < StockContinueChance) {
+                var nextStock = CreateSymbol<SimpleStock>("stock", new Vector3(0, 1, 0));
+                nextStock.Initialize(width, depth, wallStyle, roofStyle);
+                nextStock.Generate(BuildDelay);
             } else {
-                SimpleRoof nextRoof = CreateSymbol<SimpleRoof>("roof", new Vector3(0, 1, 0));
-                nextRoof.Initialize(Width, Depth, roofStyle,wallStyle);
-                nextRoof.Generate(buildDelay);
+                var nextRoof = CreateSymbol<SimpleRoof>("roof", new Vector3(0, 1, 0));
+                nextRoof.Initialize(width, depth, roofStyle,wallStyle);
+                nextRoof.Generate(BuildDelay);
             }
         }
     }
