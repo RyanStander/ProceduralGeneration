384252c9a6123c69d5907c5fefbc65641439e9a2
Ryan <stander.ryan@gmail.com>
2022-06-15 17:39:23 +0200

    Lsystem visualiser

---
+++ Assets/Scripts/TownGeneration/AgentParameters.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace TownGeneration
+{
+    public class AgentParameters
+    {
+        //the position that we will add direction to of the agent
+        public Vector3 position;
+        //where we want to go in which direction
+        public Vector3 direction;
+        //length of the movement
+        public int length;
+    }
+}
